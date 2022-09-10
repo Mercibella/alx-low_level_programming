4980a73cb98eba1b9cf8c921bc0f5d77103e5a2a
Mercibella <mercyachara@gmail.com>
2022-09-10 13:49:46 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,9 @@ int main(void)
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' && letter != 'q')						putchar(letter);
+if (letter != 'e' && letter != 'q')						
+	
+putchar(letter);
 }
 putchar('\n');
 return (0);
