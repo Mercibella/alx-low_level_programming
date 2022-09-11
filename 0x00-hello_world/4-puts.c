bcfb5c1ba67ed7647b4cf5c3b17c81664ed77fc2
Mercibella <mercyachara@gmail.com>
2022-09-11 00:04:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-/** main- Serves as the entry point for the program 
-* By default returns zero if no erros otherwise returns non-zero value. */
-int main(void)
-
-{	
-puts("\"Programming is like building a multilingual puzzle");	return (0);
-}   
+#include <stdio.h>                                                                                                          
+/**                                                                                                                         
+ *
+ * * main - Entry point                                                         * Return: Always 0 (Success)                                                
+*/                                                                          
+int main(void)                                                            
+{                                                                               puts("\"Programming is like building a multilingual puzzle");           
+return (0);                                                            
+}       
