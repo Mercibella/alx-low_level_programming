8696cc3a4a87cc8f2eb80589dd733d6fa42ce993
Mercibella <mercyachara@gmail.com>
2022-09-10 19:31:18 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,9 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
- *
- *  * main - Prints the lowercase alphabet in reverse.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+* main - Prints the lowercase alphabet in reverse.
+* Return: Always 0.
+*/
 int main(void)
 {
 char letter;
