af4b5cd4088e3db0ab144202db99e9d32d1aeedf
Mercibella <mercyachara@gmail.com>
2022-09-10 20:10:54 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,14 @@ int main(void)
 int num;
 for (num = 0; num <= 9; num++)
 {
-putchar((num % 10) + '0');							if (num == 9)
+putchar((num % 10) + '0');
+/**
+* main - prints combination of single-digit number.
+* Return: Always 0.
+*/
+if (num == 9)
 continue;
-
-     putchar(',');
+putchar(',');
 putchar(' ');
 }
 putchar('\n');
