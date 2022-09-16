608bbe2847eebf48088d093c38255580d9aecf30
Mercibella <mercyachara@gmail.com>
2022-09-16 07:18:43 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,16 +14,16 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	 else
-	 {
-		 for (i = 0; i < n; i++)
-		 {
-			 for (j = 0; j < i; j++)
-			 {
-				 _putchar(' ')
-			 }
-			 _putchar('\\');
-			 _putchar('\n');
-		 }
-	 }
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
